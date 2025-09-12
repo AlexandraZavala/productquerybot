33d7fe66514db62b909a4305d076c2a97f62fbd1
AlexandraZavala <alexandra.zavala@pucp.edu.pe>
2025-09-12 14:06:41 -0500

    add graph

---
+++ app/agents/graph.py
@@ -0,0 +1,28 @@
+from langgraph.graph import StateGraph, END 
+from .responder import AgentResponder
+from .retriever import AgentRetriever
+from schema import StateMultiAgent 
+from rag.store import VectorStore 
+
+
+class MultiAgentGraph(StateGraph[StateMultiAgent]):
+    def __init__(self):
+        self.vector_store = VectorStore(persist_directory="vector_store")
+        self.retriever = AgentRetriever(self.vector_store)
+        self.responder = AgentResponder()
+        self.graph = StateGraph(StateMultiAgent)
+        self.graph.add_node("retrieve", self.retriever)
+        self.graph.set_entry_point("retrieve")
+        self.graph.add_node("respond", self.responder)
+        self.graph.add_edge("retrieve", "respond")
+        self.graph.add_edge("respond", END)
+        self.app = self.graph.compile()
+    
+    def run (self, state: StateMultiAgent) -> StateMultiAgent:
+        if self.vector_store.has_content():
+            print("El vector store tiene contenido.")
+        else:
+            print("El vector store está vacío.")
+        return self.app.invoke(state)
+
+
